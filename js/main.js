73f61ab3514f3841e6410bea76ff8c8cc6afc57a
NadezhdaObsholova <obsholova.n@gmail.com>
2023-06-26 23:35:37 +0400

    добавление массива комментариев, сокращение кода

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
-//ИСХОДНЫЙ ДАННЫЕ
-
-//максимальное число фотогафий
 const maxNumberOfPotos = 25;
+const minLikes = 15;
+const maxLikes = 200;
 
-//массив с описанием фотографий
 const descriptions = [
   'природа',
   'горы',
@@ -14,7 +12,6 @@ const descriptions = [
   'международный праздник'
 ];
 
-//массив с именами авторов комментарев
 const namesOfAuthors = [
   'Александр',
   'Иван',
@@ -23,27 +20,9 @@ const namesOfAuthors = [
   'Олег',
   'София',
   'Варвара',
-  'Алиса',
-  'Виктория',
-  'Полина',
-  'Ева',
-  'Данила',
-  'Павел',
-  'Надежда',
-  'Анна',
-  'Людмила',
-  'Виктор',
-  'Алексей',
-  'Степан',
-  'Николай',
-  'Ирина',
-  'Григорий',
-  'Лариса',
-  'Галина',
-  'Федор'
+  'Алиса'
 ];
 
-//массив с сообщениями
 const messages = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -53,24 +32,13 @@ const messages = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
-//ВСПОМОГАТЕЛЬНЫЕ ФУНКЦИИ
-
-//функиця получения рандомного числа в диапазоне
-const createRandomInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-  const result = Math.random() * (upper - lower + 1) + lower;
+const createRandomInteger = (min, max) => {
+  const result = Math.random() * (max - min) + min;
   return Math.floor(result);
 };
 
-///выбор рандомного значения из массива
 const getRandomArrayElement = (elements) => elements[createRandomInteger(0, elements.length - 1)];
 
-//РЕШЕНИЕ ЗАДАЧИ
-
-//1. СОЗДАНИЕ ДАННЫХ КОММЕНТАРИЕВ
-
-//создание id комментария (любое неповторяющееся число в диапазоне 0 - 9999)
 function createCommentId(min, max) {
   const previousId = [];
 
@@ -89,7 +57,6 @@ function createCommentId(min, max) {
 
 const getCommentId = createCommentId(0, 9999);
 
-//выбор случайного аватара
 const createAvatar = function () {
   return function () {
     const avatarId = createRandomInteger(1, 6);
@@ -98,7 +65,6 @@ const createAvatar = function () {
 };
 const getAvatar = createAvatar();
 
-//создание объекта комментария
 function getCommentData() {
   return {
     id: getCommentId(),
@@ -108,12 +74,6 @@ function getCommentData() {
   };
 }
 
-//получение массива объектов комментариев
-const comments = Array.from({ length: maxNumberOfPotos }, getCommentData);
-
-//2. СОЗДАНИЕ ДАННЫХ ФОТОГРАФИЙ
-
-//функция получения id фотогафии
 const createId = function () {
   let lastId = 0;
   return function () {
@@ -123,7 +83,6 @@ const createId = function () {
 };
 const getId = createId();
 
-//функция получения url фотографии
 const createUrl = function () {
   let lastId = 0;
   return function () {
@@ -134,26 +93,26 @@ const createUrl = function () {
 
 const getUrl = createUrl();
 
-//получение количества лайков
-const getLikes = function () {
-  return createRandomInteger(15, 200);
-};
-
-//получение случайного описания из массива
 const getDescription = function () {
   return createRandomInteger(0, descriptions.length - 1);
 };
 
-//создание объекта данных фотографии
 function getPhotoData() {
   return {
     id: getId(),
     url: getUrl(),
     description: descriptions[getDescription()],
-    likes: getLikes(),
-    comments: getRandomArrayElement(comments),
+    likes: createRandomInteger(minLikes, maxLikes),
+    comments: Array.from({ length: createRandomInteger(0, 10) }, getCommentData),
   };
 }
 
-//получение массива объектов данных фотографий
-const photos = Array.from({ length: maxNumberOfPotos }, getPhotoData);
+const photos = [];
+
+function getPhotos() {
+  for (let i = 0; i < maxNumberOfPotos; i++){
+    photos.push(getPhotoData());
+  }
+}
+
+getPhotos();
